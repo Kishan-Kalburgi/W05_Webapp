88b1d87347f59949d6d57088991b8f866360a97f
Denise Case <dcase@nwmissouri.edu>
2017-02-13 11:59:54 -0600

    update

--- getWeather.js
+++ getWeather.js
@@ -1,4 +1,11 @@
-var getWeather = require('./weatherReader.js')('Maryville, Missouri');
+// invoke single function directly
+require('./weatherReader.js')('Maryville, Missouri');
+
+// invoke single function
+var noah = require('./weatherReader.js');
+noah('Maryville, Missouri');
+
+// invoke object with multiple functions
 var noaa = require('./weatherReader2.js');
 noaa.getTemp('Maryville, MO');
 noaa.getHumidity('Maryville, MO');
