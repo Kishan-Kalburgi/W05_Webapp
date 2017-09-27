a8eb2c539b6218ad1fde48a447cda03cf2cc3746
Kishan Salburgi Srinivas <S530468@nwmissouri.edu>
2017-09-27 13:50:59 -0500

    node app - customize the message

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ http.createServer(requestListener).listen(8081);
 function requestListener(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write("<!DOCTYPE html>");
-    response.write("<html><head><title>W05</title></head>");
-    response.write("<body><h1>Hello World!!</h1></body></html>");
+    response.write("<html><head><title>Hi</title></head>");
+    response.write("<body><h1>This is Kishan Kalburgi Srinivas</h1></body></html>");
+    response.write("<body><h1>Have nice Day!!</h1></body></html>");
     response.end();
 }
 
